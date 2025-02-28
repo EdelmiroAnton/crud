c1d08596abab0ec2439e5b54e6670e85fd313d81
Edelmiro Anton <anton.edelmiro@gmail.com>
2025-02-28 21:09:19 +0100

    add component to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import UserName from "./components/UserName";
+
+const App = () => {
+  return (
+    <>
+      <h1>Data Base Information</h1>
+      <UserName />
+    </>
+  );
+};
+
+export default App;
