c20d4cd091f15a46b0ffdb66682226b77d9f2b6a
Edelmiro Anton <anton.edelmiro@gmail.com>
2025-03-01 20:47:45 +0100

    add UserAge component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import UserName from "./components/UserName";
 import UserLastname from "./components/UserLastname";
+import UserAge from "./components/UserAge";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <h1>Data Base Information</h1>
       <UserName />
       <UserLastname />
+      <UserAge />
     </>
   );
 };
