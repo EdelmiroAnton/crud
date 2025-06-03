f27e1d276944d3535c8406a295d8c9b7ffa63267
EdelmiroAnton <anton.edelmiro@gmail.com>
2025-06-03 08:38:14 +0200

    create server and get method with express

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,20 @@ const db = mysql.createConnection({
 });
 console.log("Connected to the databasee...");
 
+// create server
+const app = express();
+const port = 3000;
+app.use(cors());
 
+app.get("/", (req, res) => {
+  db.query("SELECT * FROM users", (err, result) => {
+    if (err) console.error(`Error message: ${err.message}`);
+    res.send(result);
+    console.log(result);
+    console.log("Data fetched successfully");
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
