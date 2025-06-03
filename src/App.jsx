69a59f419b36702f61328706da0cd7da3350c647
EdelmiroAnton <anton.edelmiro@gmail.com>
2025-06-03 17:57:50 +0200

    get users with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
+import axios from "axios";
 
 const BASE_URL = "http://localhost:3000/users/";
 
@@ -6,34 +7,68 @@ const BASE_URL = "http://localhost:3000/users/";
 
 const App = () => {
   const [user, setUser] = useState([]);
+  // const [newName, setNewName] = useState("")
+  // const [newLastName, setLastNewName] = useState("")
+  // const [NewAge, setNewAge] = useState("")
+  // const [newemail, setNewEmail] = useState("")
+  const [newUser, setNewUser] = useState({
+    first_name: "",
+    last_name: "",
+    age: "",
+    email: "",
+  });
 
+  // GET USERS
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const result = await fetch(BASE_URL);
-        const data = await result.json();
-        setUser(data);
-        console.log(data);
-      } catch (error) {
-        console.log(
-          `Error at getting users info. Error Message: ${error.message}`
-        );
+        const response = await axios.get(BASE_URL);
+        setUser(response.data);
+      } catch (err) {
+        console.err(`Error message: ${err}`);
       }
     };
     getUsers();
   }, []);
 
+  //CREATE USERS
+
+  // const handleCreate = async (e) => {
+  //   const results = await fetch(`${BASE_URL}add`, {
+  //     method: "POST",
+  //   });
+  //   console.log(results);
+
+  //   const data = await results.json({});
+  //   first_name: newUser.first_name,
+  //     // last_name: newUser.last_name,
+  //     // age: newUser.age,
+  //     // email: newUser.email,
+
+  //     console.log(data);
+  // };
+
   return (
-    <div>
-      {user.map((el) => (
-        <ul key={el.id}>
-          <li>{el.first_name}</li>
-          <li>{el.last_name}</li>
-          <li>{el.age}</li>
-          <li>{el.email}</li>
-        </ul>
-      ))}
-    </div>
+    <>
+      <div>
+        {user.map((el) => (
+          <ul key={el.id}>
+            <li>{el.first_name}</li>
+            <li>{el.last_name}</li>
+            <li>{el.age}</li>
+            <li>{el.email}</li>
+          </ul>
+        ))}
+      </div>
+
+      {/* <label htmlFor="new_name">New Name</label>
+      <input
+        id="new_name"
+        type="text"
+        onChange={(e) => setNewUser({ first_name: e.target.value })}
+      />
+      <button onClick={handleCreate}>CREATE</button> */}
+    </>
   );
 };
 
