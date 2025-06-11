54d88f7ec190727dfa7da5d55a1c3224bd10ac6d
EdelmiroAnton <anton.edelmiro@gmail.com>
2025-06-11 12:23:55 +0200

    axios.post working

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import React, { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const BASE_URL = "http://localhost:3000/users/";
 
-//SHOW DDBB INFO
-
 const App = () => {
   const [user, setUser] = useState([]);
-  // const [newName, setNewName] = useState("")
-  // const [newLastName, setLastNewName] = useState("")
-  // const [NewAge, setNewAge] = useState("")
-  // const [newemail, setNewEmail] = useState("")
   const [newUser, setNewUser] = useState({
     first_name: "",
     last_name: "",
@@ -25,28 +19,21 @@ const App = () => {
         const response = await axios.get(BASE_URL);
         setUser(response.data);
       } catch (err) {
-        console.err(`Error message: ${err}`);
+        console.error(`Error message: ${err}`);
       }
     };
     getUsers();
-  }, []);
+  }, [newUser]);
 
   //CREATE USERS
-
-  // const handleCreate = async (e) => {
-  //   const results = await fetch(`${BASE_URL}add`, {
-  //     method: "POST",
-  //   });
-  //   console.log(results);
-
-  //   const data = await results.json({});
-  //   first_name: newUser.first_name,
-  //     // last_name: newUser.last_name,
-  //     // age: newUser.age,
-  //     // email: newUser.email,
-
-  //     console.log(data);
-  // };
+  const handleCreateUsers = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}add`, newUser);
+      console.log(response.data);
+    } catch (err) {
+      console.error(`Error message: ${err}`);
+    }
+  };
 
   return (
     <>
@@ -61,13 +48,34 @@ const App = () => {
         ))}
       </div>
 
-      {/* <label htmlFor="new_name">New Name</label>
+      <label htmlFor="new_name">Name</label>
       <input
         id="new_name"
         type="text"
-        onChange={(e) => setNewUser({ first_name: e.target.value })}
+        onChange={(e) => setNewUser({ ...newUser, first_name: e.target.value })}
       />
-      <button onClick={handleCreate}>CREATE</button> */}
+      <label htmlFor="last_name">Last Name</label>
+      <input
+        id="last_name"
+        type="text"
+        onChange={(e) => setNewUser({ ...newUser, last_name: e.target.value })}
+      />
+
+      <label htmlFor="age">Age</label>
+      <input
+        id="age"
+        type="number"
+        onChange={(e) => setNewUser({ ...newUser, age: e.target.value })}
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="email"
+        onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+      />
+
+      <button onClick={handleCreateUsers}>CREATE</button>
     </>
   );
 };
